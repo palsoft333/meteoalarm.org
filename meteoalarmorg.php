2bbba0e31d46296d09daa0630a2d6080367bf335
palsoft333 <pavol.svantner@gmail.com>
2024-11-15 07:51:16 +0100

    Change to simple_html_dom
    - parsing changes as the Meteoalarm website RSS changed their output format
    - moved to simple html dom parsing

--- meteoalarmorg.php
+++ meteoalarmorg.php
@@ -1,4 +1,6 @@
 <?
+include("libs/simple_html_dom.php");
+header('Content-Type: application/json');
 if($_GET["country"]) {
     $c = strtolower($_GET["country"]);
     $iso = array("at"=>"austria", "be"=>"belgium", "ba"=>"bosnia-herzegovina", "bg"=>"bulgaria", "hr"=>"croatia", "cy"=>"cyprus", "cz"=>"czechia", "dk"=>"denmark", "fi"=>"finland", "fr"=>"france", "de"=>"germany", "ee"=>"estonia", "gr"=>"greece", "hu"=>"hungary", "is"=>"iceland", "ie"=>"ireland", "il"=>"israel", "it"=>"italy", "lv"=>"latvia", "lt"=>"lithuania", "lu"=>"luxembourg", "mt"=>"malta", "md"=>"moldova", "me"=>"montenegro", "nl"=>"netherlands", "mk"=>"republic-of-north-macedonia", "no"=>"norway", "pl"=>"poland", "pt"=>"portugal", "ro"=>"romania", "rs"=>"serbia", "sk"=>"slovakia", "si"=>"slovenia", "es"=>"spain", "se"=>"sweden", "ch"=>"switzerland", "gb"=>"united-kingdom");
@@ -9,6 +11,28 @@ if($_GET["country"]) {
     $json = json_encode($xml);
     $meteo = json_decode($json,TRUE);
 
+    $alertTypes = [
+        1 => "Wind",
+        2 => "Snow-Ice",
+        3 => "Thunderstorm",
+        4 => "Fog",
+        5 => "High temperature",
+        6 => "Low temperature",
+        7 => "Coastal event",
+        8 => "Forest Fire",
+        9 => "Avalanches",
+        10 => "Rain",
+        12 => "Flooding",
+        13 => "Rain Flood"
+    ];
+
+    $alertLevels = [
+        1 => ["color" => "Green", "desc" => "Minor"],
+        2 => ["color" => "Yellow", "desc" => "Moderate"],
+        3 => ["color" => "Orange", "desc" => "Severe"],
+        4 => ["color" => "Red", "desc" => "Extreme"]
+    ];
+
     $p=0;
     $meteo_events=array();
     foreach($meteo["channel"]["item"] as $region) {
@@ -17,55 +41,70 @@ if($_GET["country"]) {
             $pubDate = $region["pubDate"];
             $desc = $region["description"];
             $link = $region["link"];
-            $events = explode("alt=", $desc);
-            array_shift($events);
-            foreach($events as $event) {
-                $from_start = strpos($event, "<i>");
-                $from_end = strpos($event, "</i>", $from_start);
-                $until_start = strpos($event, "<i>", $from_end);
-                $until_end = strpos($event, "</i>", $until_start);
-                $event_from = trim(strip_tags(substr($event, $from_start, $from_end-$from_start)));
-                $event_until = trim(strip_tags(substr($event, $until_start, $until_end-$until_start)));
-                $awt_start = strpos($event, '"awt:')+5;
-                $awt_end = strpos($event, ' ', $awt_start);
-                $level_start = strpos($event, 'level:', $awt_end)+6;
-                $level_end = strpos($event, '"', $level_start);
-                $awt = trim(strip_tags(substr($event, $awt_start, $awt_end-$awt_start)));
-                $level = trim(strip_tags(substr($event, $level_start, $level_end-$level_start)));
-                $description = explode("<td>", $event);
-                $description = trim(strip_tags($description[2]));
-                if($awt==1) $alert_type_desc = "Wind";
-                elseif($awt==2) $alert_type_desc = "Snow-Ice";
-                elseif($awt==3) $alert_type_desc = "Thunderstorm";
-                elseif($awt==4) $alert_type_desc = "Fog";
-                elseif($awt==5) $alert_type_desc = "High temperature";
-                elseif($awt==6) $alert_type_desc = "Low temperature";
-                elseif($awt==7) $alert_type_desc = "Coastal event";
-                elseif($awt==8) $alert_type_desc = "Forest Fire";
-                elseif($awt==9) $alert_type_desc = "Avalanches";
-                elseif($awt==10) $alert_type_desc = "Rain";
-                elseif($awt==12) $alert_type_desc = "Flooding";
-                elseif($awt==13) $alert_type_desc = "Rain Flood";
-                if($level==1) { $alert_color = "Green"; $alert_desc = "Minor"; }
-                elseif($level==2) { $alert_color = "Yellow"; $alert_desc = "Moderate"; }
-                elseif($level==3) { $alert_color = "Orange"; $alert_desc = "Severe"; }
-                elseif($level==4) { $alert_color = "Red"; $alert_desc = "Extreme"; }
-                $meteo_events[$p]["locality"] = $title;
-                $meteo_events[$p]["valid_from"] = $event_from;
-                $meteo_events[$p]["valid_till"] = $event_until;
-                $meteo_events[$p]["alert_type"] = $awt;
-                $meteo_events[$p]["alert_type_desc"] = $alert_type_desc;
-                $meteo_events[$p]["alert_level"] = $level;
-                $meteo_events[$p]["alert_color"] = $alert_color;
-                $meteo_events[$p]["alert_desc"] = $alert_desc;
-                $meteo_events[$p]["description"] = $description;
-                $meteo_events[$p]["condition_icon"] = "https://feeds.meteoalarm.org/images/rss/wflag-l".$level."-t".$awt.".png";
-                $meteo_events[$p]["pubDate"] = $pubDate;
-                $meteo_events[$p]["link"] = $link;
-                $p++;
+            $html = str_get_html($desc);
+
+            $events = [];
+            foreach ($html->find('td[data-awareness-level]') as $event) {
+                $currentRow = $event->parent();
+                $nextRow = $currentRow->next_sibling();
+                $events[] = [
+                    'event' => $currentRow->outertext,
+                    'details' => $nextRow ? $nextRow->outertext : null
+                ];
+            }
+
+            foreach ($events as $eventData) {
+                $eventHtml = str_get_html($eventData['event']);
+                $detailsHtml = str_get_html($eventData['details']);
+
+                $fromDate = null;
+                $untilDate = null;
+                if ($eventHtml->find('td', 1)) {
+                    $fromUntilText = $eventHtml->find('td', 1)->plaintext;       
+                    preg_match('/From:\s*(\S+)/', $fromUntilText, $fromMatch);
+                    preg_match('/Until:\s*(\S+)/', $fromUntilText, $untilMatch);
+                    $fromDate = $fromMatch[1] ?? null;
+                    $untilDate = $untilMatch[1] ?? null;
+                }
+
+                $awt = null;
+                $level = null;
+                if ($eventHtml->find('td[data-awareness-level]', 0)) {
+                    $awtText = $eventHtml->find('td[data-awareness-level]', 0)->innertext;
+                    preg_match('/awt:(\d+)/', $awtText, $awtMatch);
+                    preg_match('/level:(\d+)/', $awtText, $levelMatch);
+                    $level = $awtMatch[1] ?? null;
+                    $awt = $levelMatch[1] ?? null;
+                }
+
+                $description = null;
+                if ($detailsHtml->find('td', 1)) {
+                    $descriptionText = $detailsHtml->find('td', 1)->plaintext;
+                    $description = trim($descriptionText);
+                }
+
+                $alert_type_desc = $alertTypes[$awt] ?? "Unknown";
+                $alert_color = $alertLevels[$level]['color'] ?? "Unknown";
+                $alert_desc = $alertLevels[$level]['desc'] ?? "Unknown";
+
+                $meteo_events[] = [
+                    "locality" => $title,
+                    "valid_from" => $fromDate,
+                    "valid_till" => $untilDate,
+                    "alert_type" => $awt,
+                    "alert_type_desc" => $alert_type_desc,
+                    "alert_level" => $level,
+                    "alert_color" => $alert_color,
+                    "alert_desc" => $alert_desc,
+                    "description" => $description,
+                    "condition_icon" => "https://feeds.meteoalarm.org/images/rss/wflag-l".$level."-t".$awt.".png",
+                    "pubDate" => $pubDate,
+                    "link" => $link
+                ];
             }
         }
     }
+
     $json = json_encode($meteo_events);
     echo $json;
 }
